d0aa613c310aae4843c19224686fad730ec18103
TheRohinth <rohinth.1901202@gmail.com>
2021-09-19 14:31:46 +0530

    Added ResNet50 architecture's Basic Code

--- src/DL_model_files/blocks.py
+++ src/DL_model_files/blocks.py
@@ -1,13 +1,42 @@
-def convolutional_block():
-    '''
-        TODO:
-            ____
-    '''
-    pass
+import utils
 
-def identity_block():
-    '''
-        TODO:
-            ____
-    '''
-    pass
+def convolutional_block(X):
+    X_skip = X
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+
+    X_skip = Conv2D()(X_skip)
+    X_skip = BatchNormalization()(X_skip)
+
+    X = Add()([X, X_skip])
+    X = Activation('relu')(X)
+    
+    return X
+
+def identity_block(X):
+    X_skip = X
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+    X = Activation('relu')(X)
+
+    X = Conv2D()(X)
+    X = BatchNormalization()(X)
+
+    X = Add()([X, X_skip])
+    X = Activation('relu')(X)
+    
+    return X
--- src/DL_model_files/init_resnet_50.py
+++ src/DL_model_files/init_resnet_50.py
@@ -5,20 +5,24 @@ def ResNet50(input_shape, classes):
     X_input = Input(input_shape)
     X = ZeroPadding2D()
 
+    # 1 layer
     X = Conv2D()(X)
     X = BatchNormalization()(X)
     X = Activation('relu')(X)
     X = MaxPooling2D()(X)
 
+    # 9 layers
     X = convolutional_block()
     X = identity_block()
     X = identity_block()
 
+    # 12 layers
     X = convolutional_block()
     X = identity_block()
     X = identity_block()
     X = identity_block()
 
+    # 18 layers
     X = convolutional_block()
     X = identity_block()
     X = identity_block()
@@ -26,15 +30,18 @@ def ResNet50(input_shape, classes):
     X = identity_block()
     X = identity_block()
 
+    # 9 layers
     X = convolutional_block()
     X = identity_block()
     X = identity_block()
 
+    # 1 layer
     X = AveragePooling2D()(X)
 
     X = Flatten()(X)
     X = Dense(activation='softmax')(X)
-    
+
+    # 1 + 9 + 12 + 18 + 9 + 1 = 50
     model = Model(inputs = X_input, outputs = X, name='ResNet50')
 
     return model
